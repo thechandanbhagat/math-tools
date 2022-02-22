86d572461e589c7444924cb36562d675c91f4ecb
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-02-22 23:12:10 +0545

    themes integration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import AppFooter from './comp/appfooter/appfooter';
+import AppHeader from './comp/appheader/appheader';
+import AppMenu from './comp/appmenu/appmenu';
+import Content from './comp/content/content';
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="wrapper">
+      <AppHeader />
+      <AppMenu />
+      <Content />
+      <AppFooter />
+  </div>
   );
 }
 
---
+++ src/comp/appfooter/appfooter.js
@@ -0,0 +1,15 @@
+
+ 
+function App() {
+  return (
+    <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://math-tools.chandanbhagat.com.np">Math Tools</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b>1.0.0
+    </div>
+  </footer>
+  );
+}
+ 
+export default App
