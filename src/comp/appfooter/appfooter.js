eec92d37cf3a3bfbb15ef5c4fc4462c883604cf9
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-02-26 19:33:02 +0545

    changes in footer

--- src/comp/appfooter/appfooter.js
+++ src/comp/appfooter/appfooter.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <footer className="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://math-tools.chandanbhagat.com.np">Math Tools</a>.</strong>
+    <strong>Copyright &copy; 2022 <a href="https://math-tools.chandanbhagat.com.np">Math Tools</a>.</strong>
     All rights reserved.
     <div className="float-right d-none d-sm-inline-block">
       <b>Version</b>1.0.0
