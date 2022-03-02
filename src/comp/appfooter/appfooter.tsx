7c1b313af7c3c6cea28590b26f90a03eed622bd2
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-03-02 13:39:10 +0545

    updated the version

--- src/comp/appfooter/appfooter.tsx
+++ src/comp/appfooter/appfooter.tsx
@@ -6,7 +6,7 @@ function App() {
     <strong>Copyright &copy; 2022 <a href="https://math-tools.chandanbhagat.com.np">Math Tools</a>. </strong>
     All rights reserved to <a href="https://chndanbhagat.com.np">Chandan Bhagat</a>.
     <div className="float-right d-none d-sm-inline-block">
-      <b>Version</b>1.0.0
+      <b>Version </b>1.0.0-alpha
     </div>
   </footer>
   );
