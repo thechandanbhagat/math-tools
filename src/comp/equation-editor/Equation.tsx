c7e48288db2ae5aaa18b106a53edc1faeb720342
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-03-02 13:36:44 +0545

    cleanups

--- src/comp/equation-editor/Equation.tsx
+++ src/comp/equation-editor/Equation.tsx
@@ -5,63 +5,56 @@ const equationWrapperStyle: CSSProperties = {
   fontSize: "48px",
   position: "relative",
   display: "table",
-  
-  marginTop:"2%",
+
+  marginTop: "5%",
   left: "50%",
-  transform: "translate(-50%, -50%)"
+  transform: "translate(-50%, -50%)",
 };
 
 const equationOutputStyle: CSSProperties = {
   fontSize: "16px",
   color: "rgb(232, 62, 140)",
-  textAlign: "center"
+  textAlign: "center",
 };
 
 const placeholderOutputStyle: CSSProperties = {
   fontSize: "20px",
   display: "table",
 
- marginLeft:"auto",
- marginRight:"auto",
-  textAlign: "center"
+  marginLeft: "auto",
+  marginRight: "auto",
+  textAlign: "center",
 };
 
-
 function Equation() {
-function handleSubmit(event:any) {
-  event.preventDefault();
-  setEqn(TextEqn);
-  console.log(TextEqn);
-  console.log("eqn => "+Eqn);
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    setEqn(TextEqn);
+    console.log(TextEqn);
+    console.log("eqn => " + Eqn);
+  }
 
-}
-
-  const [TextEqn, setTextEqn] = useState('');
-  const [Eqn, setEqn] = useState('y=x');
-    return (
-      <div className="container-fluid">
-        <div className="row">
-
-        </div>
-          <div className="row" style={placeholderOutputStyle}>
-          Type the equation here
-          </div>
-        <div id="eq" style={equationWrapperStyle}>
-        <EquationEditor 
-      value={Eqn}
-      onChange={setEqn}
-      autoCommands="bar overline sqrt sum prod int alpha beta gamma delta epsilon zeta eta theta iota kappa lambda mu nu xi omikron pi rho sigma tau upsilon phi chi psi omega Alpha Beta Gamma Aelta Epsilon Zeta Eta Theta Iota Kappa Lambda Mu Nu Xi Omikron Pi Rho Sigma Tau Upsilon Phi Chi Psi Omega rangle langle otimes neq leq ll geq gg approx dagger angle in"
-      autoOperatorNames="sin cos tan"
-      
-    />
-        <p style={equationOutputStyle}>
-        <code>{Eqn}</code>
-       
-      </p>
-        </div>
+  const [TextEqn, setTextEqn] = useState("");
+  const [Eqn, setEqn] = useState("y=x");
+  return (
+    <div className="container-fluid">
+      <div className="row"></div>
+      <div className="row" style={placeholderOutputStyle}>
+        Type the equation here
       </div>
-    );
-  
+      <div id="eq" style={equationWrapperStyle}>
+        <EquationEditor
+          value={Eqn}
+          onChange={setEqn}
+          autoCommands="bar overline sqrt sum prod int alpha beta gamma delta epsilon zeta eta theta iota kappa lambda mu nu xi omikron pi rho sigma tau upsilon phi chi psi omega Alpha Beta Gamma Aelta Epsilon Zeta Eta Theta Iota Kappa Lambda Mu Nu Xi Omikron Pi Rho Sigma Tau Upsilon Phi Chi Psi Omega rangle langle otimes neq leq ll geq gg approx dagger angle in"
+          autoOperatorNames="sin cos tan"
+        />
+        <p style={equationOutputStyle}>
+          <code>{Eqn}</code>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Equation;
