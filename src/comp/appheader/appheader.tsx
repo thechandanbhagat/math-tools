3705d4eef11eb139516da5f832a5c9e8bda74fe2
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-03-07 03:54:31 +0545

    header UI improved

--- src/comp/appheader/appheader.tsx
+++ src/comp/appheader/appheader.tsx
@@ -1,49 +1,55 @@
 import React from "react";
-function App(){
-    return (
-        <nav className="main-header navbar navbar-expand navbar-white navbar-light">
-        {/* <!-- Left navbar links --> */}
-        <ul className="navbar-nav">
-          <li className="nav-item d-none d-sm-inline-block">
-            <a href="/" className="nav-link">Home</a>
-          </li>
-          <li className="nav-item d-none d-sm-inline-block">
-            <a href="/fraction" className="nav-link">Fractions</a>
-          </li>
-          <li className="nav-item d-none d-sm-inline-block">
-            <a href="/derivative" className="nav-link">Derivatives</a>
-          </li>
-        </ul>
-    
-        <ul className="navbar-nav ml-auto">
-          {/* <li className="nav-item">
-            <a className="nav-link" data-widget="navbar-search" href="/" role="button">
-              <i className="fas fa-search"></i>
-            </a>
-            <div className="navbar-search-block">
-              <form className="form-inline">
-                <div className="input-group input-group-sm">
-                  <input className="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search" />
-                  <div className="input-group-append">
-                    <button className="btn btn-navbar" type="submit">
-                      <i className="fas fa-search"></i>
-                    </button>
-                    <button className="btn btn-navbar" type="button" data-widget="navbar-search">
-                      <i className="fas fa-times"></i>
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </li> */}
+
+function App() {
+  return (
+    <nav className="main-header navbar navbar-expand navbar-white navbar-light elevation-5">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler order-1"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse order-3" id="navbarCollapse">
+          {/* <!-- Left navbar links --> */}
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/fraction" className="nav-link">
+                Fractions
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/derivative" className="nav-link">
+                Derivatives
+              </a>
+            </li>
+          </ul>
+        </div>
+        <ul className="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
           <li className="nav-item">
-            <a className="nav-link" data-widget="fullscreen" href="#" role="button">
+            <a
+              className="nav-link"
+              data-widget="fullscreen"
+              href="#"
+              role="button"
+            >
               <i className="fas fa-expand-arrows-alt"></i>
             </a>
           </li>
         </ul>
-      </nav>
-    );
+      </div>
+    </nav>
+  );
 }
 
 export default App;
