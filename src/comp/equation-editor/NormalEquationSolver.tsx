8c13e61851a15bc3628ddb43d1834df7cae7c73d
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-03-10 18:08:56 +0545

    removed unwanted function

--- src/comp/equation-editor/NormalEquationSolver.tsx
+++ src/comp/equation-editor/NormalEquationSolver.tsx
@@ -29,20 +29,7 @@ class NormalEquationSolver implements IEquationSolver {
     return equation.replace(regex, '$1\+$2');
   }
 
-  calculateFractions(first: string, second: string): string {
-    var a = first.split("}{")[0].replace("{", "");
-    var b = first.split("}{")[1].replace("}", "");
-    var c = second.split("}{")[0].replace("{", "");
-    var d = second.split("}{")[1].replace("}", "");
-    var numerator = parseInt(a) * parseInt(d) + parseInt(b) * parseInt(c);
-    var denominator = parseInt(b) * parseInt(d);
-    var gcd = this.gcd(numerator, denominator);
-    console.log("numerator : "+numerator);
-    console.log("denominator : "+denominator);
-    console.log("gcd : "+gcd);
-    var result = numerator / denominator;
-    return "\\frac{"+(numerator/gcd)+"}{"+(denominator/gcd)+"}";
-  }
+
 
   //calculate the gcd of two numbers
     gcd(a: number, b: number): number {
@@ -51,26 +38,6 @@ class NormalEquationSolver implements IEquationSolver {
         else
             return this.gcd(b, a % b);
     }
-
-
-  ///function to display all the prime number between 1 to 100
-    primeNumber(number : number) : any[]{
-        var prime = [];
-        for (var i = 2; i <= number; i++) {
-            var flag = 0;
-            for (var j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    flag = 1;
-                }
-            }
-            if (flag == 0) {
-                prime.push(i);
-            }
-        }
-        return prime;
-    }
-    //latex string solve the math fraction numbers
-    
 
 }
 export default NormalEquationSolver;
